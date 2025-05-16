65e99409b41d2297ee739544f574d56b88793a7b
mrauthentik <umokeuchenna2020@gmail.com>
2025-05-16 15:54:06 +0100

    refactor: update form inputs to use formik values for better state management

--- task-manager/src/App.tsx
+++ task-manager/src/App.tsx
@@ -9,28 +9,28 @@ const initialValues = {
 }
 const App = () =>{
  
- const formik = useFormik({
+ const {values, handleBlur,handleChange,handleSubmit} = useFormik({
      initialValues,
      onSubmit: (values) => {
        console.log('Form data', values);
      },
   })
-  console.log(formik)
+
  
   return (
     <div>
      <form className="signup_form">
       <label htmlFor="name">Name</label>
-      <input type="text" name='name' />
+      <input type="text" name='name' value={values.name} />
 
       <label htmlFor="email">Email</label>
-      <input type="email" name='email' />
+      <input type="email" name='email' value={values.email} />
 
       <label htmlFor="password">Password</label>
-      <input type="password" name='password' />
+      <input type="password" name='password' value={values.password} />
 
       <label htmlFor="cpassword">Confirm Password</label>
-      <input type="password" name='cpassword' />
+      <input type="password" name='cpassword' value={values.cpassword}/>
       
      </form>
     </div>
