b3466833781b7299ac740f19f09569703ee7ec82
mrauthentik <umokeuchenna2020@gmail.com>
2025-05-16 15:38:15 +0100

    refactor: replace welcome message with signup form in App component

--- task-manager/src/App.tsx
+++ task-manager/src/App.tsx
@@ -5,7 +5,20 @@ function App() {
  
   return (
     <>
-     <h1>Welcome to my Formik Form validation</h1>
+     <form className="signup_form">
+      <label htmlFor="name">Name</label>
+      <input type="text" name='name' />
+
+      <label htmlFor="email">Email</label>
+      <input type="email" name='email' />
+
+      <label htmlFor="password">Password</label>
+      <input type="password" name='password' />
+
+      <label htmlFor="cpassword">Confirm Password</label>
+      <input type="password" name='cpassword' />
+      
+     </form>
     </>
   )
 }
