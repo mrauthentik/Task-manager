f115a9a5c829fd3648f0036b87376622752c2562
mrauthentik <umokeuchenna2020@gmail.com>
2025-05-16 16:50:44 +0100

    refactor: simplify form handling by removing useFormik and using Field components

--- task-manager/src/App.tsx
+++ task-manager/src/App.tsx
@@ -1,4 +1,4 @@
-import { useFormik, Formik, Form, Field} from 'formik'
+import { Formik, Form, Field} from 'formik'
 import './App.css'
 import { signUpSchema } from './schemas'
 
@@ -9,48 +9,37 @@ const initialValues = {
   cpassword: ''
 }
 const App = () =>{
- 
- const {values,errors,touched, handleBlur,handleChange,handleSubmit} = useFormik({
-     initialValues,
-     validationSchema: signUpSchema,
-     onSubmit: (values, actions) => {
-      actions.resetForm()
-      console.error(errors)
-       console.log('Form data', values);
-
-     },
-  })
-
-  const onSubmit = (values:any, actions:any)=>{
-    console.log(values)
-    actions.resetForm()
-  }
-
- 
+ const handleSubmit = (values: any, actions:any) => {
+  console.log(values);
+  actions.resetForm();
+  alert('Form submitted successfully');
+}
   return (
     <div>
       <h2>Welcome to my Formik Page</h2>
-      <Formik initialValues={initialValues} validationSchema={signUpSchema} onSubmit={onSubmit}>
+      <Formik initialValues={initialValues} validationSchema={signUpSchema} onSubmit={handleSubmit} >
+      {({errors,touched}) => (
+        <Form className="signup_form">
+          <label htmlFor="name">Name</label>
+          {/* <input type="text" name='name' value={values.name} onChange={handleChange} onBlur={handleBlur} /> */}
+          <Field type="text" name="name" />
+          {errors.name && touched.name && <p className='error'>{errors.name}</p>}
 
-     <form className="signup_form" onSubmit={handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input type="text" name='name' value={values.name} onChange={handleChange} onBlur={handleBlur} />
-      {errors.name && touched.name && <p className='error'>{errors.name}</p>}
+          <label htmlFor="email">Email</label>
+          <Field type="email" name="email" />
+          {errors.email && touched.email && <p className='error'>{errors.email} </p>}
 
-      <label htmlFor="email">Email</label>
-      <input type="email" name='email' value={values.email} onChange={handleChange} onBlur={handleBlur} />
-      {errors.email && touched.email && <p className='error'>{errors.email} </p>}
+          <label htmlFor="password">Password</label>
+          <Field type="password" name="password" />
+          {errors.password && touched.password && <p className='error'>{errors.password} </p>}
 
-      <label htmlFor="password">Password</label>
-      <input type="password" name='password' value={values.password} onChange={handleChange} onBlur={handleBlur} />
-      {errors.password && touched.password && <p className='error'>{errors.password} </p>}
+          <label htmlFor="cpassword">Confirm Password</label>
+          <Field type="password " name="password" />
+          {errors.cpassword && touched.cpassword && <p className='error'>{errors.cpassword} </p>}
 
-      <label htmlFor="cpassword">Confirm Password</label>
-      <input type="password" name='cpassword' value={values.cpassword} onChange={handleChange} onBlur={handleBlur}/>
-      {errors.cpassword && touched.cpassword && <p className='error'>{errors.cpassword} </p>}
-
-      <button type='submit'> Submit</button>
-     </form>
+          <button type='submit'> Submit</button>
+        </Form>
+      )}
       </Formik>
     </div>
   )
